9ba4d065a2c0ea363c00fc4ab482c5a4337feb2c
Maike Negreiros <maike_jbe@hotmail.com>
2020-09-15 20:26:36 -0300

    implemented add and remove contact logic

--- src/components/ContactBox.vue
+++ src/components/ContactBox.vue
@@ -1,15 +1,88 @@
 <template>
-  <div class="ContactBox_container">
-    <div class="ContactBox_plusIcon">
-      <PlusIcon />
+  <div>
+    <div
+      class="ContactBox_container"
+      v-for="contact in $store.state.contacts"
+      :key="contact"
+    >
+      <div class="ContactBox_plusIcon">
+        <PlusIcon />
+      </div>
+      <input
+        name="contact[]"
+        :value="contact"
+        readonly
+        class="ContactBox_input"
+      />
+      <div
+        class="ContactBox_closeIcon"
+        aria-label="Close Button"
+        @click="removeContact(contact)"
+      />
     </div>
-    <input
-      class="ContactBox_input"
-      type="text"
-      placeholder="Adicionar Contato (Telefone, email, twitter, facebook)"
-    />
+
+    <div class="ContactBox_container">
+      <div class="ContactBox_plusIcon">
+        <PlusIcon />
+      </div>
+      <input
+        name="contact[]"
+        ref="input"
+        type="text"
+        class="ContactBox_input"
+        placeholder="Adicionar Contato (Telefone, email, twitter, facebook)"
+        @focus="onInputFocus()"
+        @blur="onInputBlur()"
+        @keyup.enter="addContact()"
+      />
+      <div
+        v-if="isInputFocused"
+        class="ContactBox_enterIcon"
+        @click="addContact()"
+      >
+        <EnterIcon />
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import PlusIcon from "../assets/outline-cancel-24px.svg";
+import EnterIcon from "../assets/round-subdirectory_arrow_left-24px.svg";
+
+export default {
+  name: "ContactBox",
+  data() {
+    return {
+      isInputFocused: false,
+    };
+  },
+  components: {
+    PlusIcon,
+    EnterIcon,
+  },
+  methods: {
+    onInputFocus() {
+      this.isInputFocused = true;
+    },
+    onInputBlur() {
+      if (this.$refs.input.value !== "") return;
+      this.isInputFocused = false;
+    },
+    addContact() {
+      const { input } = this.$refs;
+      this.$store.commit("addContact", input.value);
+      this.clearInput();
+    },
+    removeContact(contact) {
+      this.$store.commit("removeContact", contact);
+    },
+    clearInput() {
+      this.$refs.input.value = "";
+    },
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 .ContactBox_container {
@@ -20,6 +93,7 @@
   border: 1px dashed #95989a;
   box-shadow: 0 3px 8px #0000001a;
 }
+
 .ContactBox_plusIcon {
   margin-right: 40px;
 }
@@ -30,19 +104,23 @@
   border: none;
   font-family: futura-book;
   outline: none;
+  color: #707070;
   &::placeholder {
     color: #cecece;
   }
 }
+
+.ContactBox_enterIcon {
+  cursor: pointer;
+}
+
+.ContactBox_closeIcon {
+  width: 15px;
+  height: 15px;
+  background-color: #8d8d8d;
+  &::before {
+    content: "x";
+    display: block;
+  }
+}
 </style>
-
-<script>
-import PlusIcon from "../assets/outline-cancel-24px.svg";
-
-export default {
-  name: "ContactBox",
-  components: {
-    PlusIcon,
-  },
-};
-</script>
