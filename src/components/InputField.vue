9b64272774313e351991cd6ec4071a2eb6b5db63
Maike Negreiros <maike_jbe@hotmail.com>
2020-09-15 10:03:24 -0300

    Fixed input font family and spacing

--- src/components/InputField.vue
+++ src/components/InputField.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="InputField_container">
     <label class="InputField_label">{{ label }}</label>
-    <input class="InputField_input" :type="type" :name="name">
+    <input class="InputField_input" :type="type" :name="name" />
   </div>
 </template>
 
@@ -9,19 +9,21 @@
 .InputField_container {
   display: inline-block;
 }
+
 .InputField_input {
-  border-radius: 20px;
+  padding: 12px 18px;
+  margin-top: 8px;
+  font-size: 14px;
   border: 1px solid #292929;
   border-radius: 8px;
-  width: 259px;
-  height: 40px;
 }
 
 .InputField_label {
   display: block;
   text-align: left;
+  font-family: futura-book;
   font-size: 12px;
-  color: #8D8D8D;
+  color: #8d8d8d;
 }
 </style>
 
@@ -32,8 +34,8 @@ export default {
     label: String,
     type: {
       type: String,
-      default: "text"
-    }
-  }
-}
+      default: "text",
+    },
+  },
+};
 </script>
