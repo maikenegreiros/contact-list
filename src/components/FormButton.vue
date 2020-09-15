fd62a943ed026811a083075d51ce1078d8e05577
Maike Negreiros <maike_jbe@hotmail.com>
2020-09-15 09:56:54 -0300

    Created button component

---
+++ src/components/FormButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <button :style="cssVars" type="button" class="FormButton_buttonElement">
+    {{ text }}
+  </button>
+</template>
+
+<style scoped>
+.FormButton_buttonElement {
+  font-family: futura-book;
+  font-size: 18px;
+  background-color: var(--bg-color);
+  color: var(--color);
+  width: 188px;
+  height: 40px;
+  border-radius: 35px;
+  border: none;
+  cursor: pointer;
+}
+</style>
+<script>
+export default {
+  name: "FormButton",
+  props: {
+    text: String,
+    bgColor: {
+      type: String,
+      default: "#ffa700",
+    },
+    color: {
+      type: String,
+      default: "#fff",
+    },
+  },
+  computed: {
+    cssVars() {
+      return {
+        "--bg-color": this.bgColor,
+        "--color": this.color,
+      };
+    },
+  },
+};
+</script>
