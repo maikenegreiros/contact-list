32ce7887b5866c1586579050767f58b9e9fa1074
Maike Negreiros <maike_jbe@hotmail.com>
2020-09-15 10:02:02 -0300

    Added buttons and enter svg icon

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,22 +1,67 @@
 <template>
-  <div>
-    <InputField name="name" label="Nome" />
-    <InputField name="lastname" label="Sobrenome" />
-    <div>
-      <ContactBox />
+  <div class="ContactForm_container">
+    <h1 class="ContactForm_title">Meus contatos</h1>
+    <div class="ContactForm_widthLimiter">
+      <div class="ContactForm_InputsContainer">
+        <InputField name="name" label="Nome" />
+        <InputField name="lastname" label="Sobrenome" />
+      </div>
+      <div class="ContactForm_ContactBoxContainer">
+        <ContactBox />
+      </div>
+    </div>
+    <div class="ContactForm_ButtonsContainer">
+      <FormButton text="Salvar" />
+      <FormButton text="Cancelar" bgColor="transparent" color="#8D8D8D" />
     </div>
   </div>
 </template>
 
+<style scoped>
+.ContactForm_title {
+  font-family: hurme-geometric-sans, sans-serif;
+  text-align: left;
+  text-transform: uppercase;
+  color: #1B3949;
+}
+
+.ContactForm_InputsContainer {
+  display: flex;
+  justify-content: space-between;
+}
+
+.ContactForm_container {
+  width: 668px;
+  padding: 39px 49px;
+  margin: auto;
+  box-shadow: 0 3px 6px #00000029;
+}
+
+.ContactForm_widthLimiter {
+  max-width: 536px;
+}
+
+.ContactForm_ContactBoxContainer {
+  margin: 35px 0;
+}
+
+.ContactForm_ButtonsContainer {
+  text-align: left;
+  margin: 52px 0;
+}
+</style>
+
 <script>
 import InputField from "./InputField.vue";
 import ContactBox from "./ContactBox.vue";
+import FormButton from "./FormButton.vue";
 
 export default {
   name: "ContactForm",
   components: {
     InputField,
-    ContactBox
+    ContactBox,
+    FormButton,
   },
 };
 </script>
