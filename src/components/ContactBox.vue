0e8d6bb5f9b5cd21c39a4c0f81ac78e1af40a38e
Maike Negreiros <maike_jbe@hotmail.com>
2020-09-15 10:45:30 -0300

    Changed p element by a input element

--- src/components/ContactBox.vue
+++ src/components/ContactBox.vue
@@ -3,35 +3,46 @@
     <div class="ContactBox_plusIcon">
       <PlusIcon />
     </div>
-    <p class="ContactBox_placeholder">Adicionar Contato (Telefone, email, twitter, facebook)</p>
+    <input
+      class="ContactBox_input"
+      type="text"
+      placeholder="Adicionar Contato (Telefone, email, twitter, facebook)"
+    />
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .ContactBox_container {
   display: flex;
   justify-content: flex-start;
   align-items: center;
   padding: 14px 17px;
-  border: 1px dashed #95989A;
-  box-shadow: 0 3px 8px #0000001A;
+  border: 1px dashed #95989a;
+  box-shadow: 0 3px 8px #0000001a;
 }
 .ContactBox_plusIcon {
   margin-right: 40px;
 }
-.ContactBox_placeholder {
+
+.ContactBox_input {
   font-size: 14px;
-  color: #CECECE;
+  width: 100%;
+  border: none;
+  font-family: futura-book;
+  outline: none;
+  &::placeholder {
+    color: #cecece;
+  }
 }
 </style>
 
 <script>
-import PlusIcon from "../assets/outline-cancel-24px.svg"
+import PlusIcon from "../assets/outline-cancel-24px.svg";
 
 export default {
   name: "ContactBox",
   components: {
     PlusIcon,
-  }
+  },
 };
 </script>
